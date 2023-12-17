f26037f10d296455fe9f7e0d3cadf60bef009c8d
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2023-12-17 11:43:17 +0100

    Update postgres_connection.py

--- quandoo_project/2.database_setup/postgres_connection.py
+++ quandoo_project/2.database_setup/postgres_connection.py
@@ -1,6 +1,6 @@
 import psycopg2
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine 
 
 # Connection to database created in docker compose file
 conn = psycopg2.connect(
@@ -18,8 +18,22 @@ engine = create_engine(f'postgresql://postgres:postgres@0.0.0.0/scrapped_data_da
 
 # Reading the result file
 df = pd.read_csv('quandoo_berlin_results.csv')
+# Populating the csv to the table
 df.to_sql('berlin_restaurants_table', engine, if_exists='replace', index=False)
 
+# Fetching the database
+cursor.execute(
+    """
+SELECT *
+FROM "berlin_restaurants_table"
+WHERE "Restaurant_location" = 'Mitte'
+LIMIT 50
+"""
+)
+rows = cursor.fetchall()
+print('The number of parts: ', cursor.rowcount)
+for row in rows:
+    print(row)
 
 # Commit the changes and close the connection to the default database
 conn.commit()
